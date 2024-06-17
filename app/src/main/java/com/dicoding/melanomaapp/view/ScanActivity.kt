85f8e99f70729dd060dc50cc138597eac6c4e542
Ganriyo <672020051@student.uksw.edu>
2024-06-18 02:39:47 +0700

    Connent Main and Scan Activity

--- app/src/main/java/com/dicoding/melanomaapp/view/ScanActivity.kt
+++ app/src/main/java/com/dicoding/melanomaapp/view/ScanActivity.kt
@@ -1,4 +1,14 @@
 package com.dicoding.melanomaapp.view
 
-class ScanActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.melanomaapp.R
+
+class ScanActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+    }
+
 }
