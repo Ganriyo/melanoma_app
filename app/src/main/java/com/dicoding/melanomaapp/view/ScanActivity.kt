3371e38141b6a6ebaa5f0104c98a346be66c2d0f
Ganriyo <672020051@student.uksw.edu>
2024-06-18 20:33:12 +0700

    Update Scan Activity

--- app/src/main/java/com/dicoding/melanomaapp/view/ScanActivity.kt
+++ app/src/main/java/com/dicoding/melanomaapp/view/ScanActivity.kt
@@ -1,14 +1,44 @@
 package com.dicoding.melanomaapp.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.melanomaapp.R
 
 class ScanActivity : AppCompatActivity() {
 
+    private lateinit var galleryButton: Button
+    private lateinit var previewImageView: ImageView
+    private val REQUEST_CODE_PICK_IMAGE = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scan)
+
+        galleryButton = findViewById(R.id.galleryButton)
+        previewImageView = findViewById(R.id.previewImageView)
+
+        val btnBack: ImageView = findViewById(R.id.btn_back)
+        btnBack.setOnClickListener {
+            finish()
+        }
+
+        galleryButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_CODE_PICK_IMAGE && resultCode == RESULT_OK && data != null) {
+            val selectedImageUri: Uri? = data.data
+            previewImageView.setImageURI(selectedImageUri)
+        }
     }
 
 }
