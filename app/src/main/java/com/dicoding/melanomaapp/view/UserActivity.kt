ae9193fe0066ab67224733810a4b41752b697142
Ganriyo <672020051@student.uksw.edu>
2024-06-20 11:39:31 +0700

    asdasd

---
+++ app/src/main/java/com/dicoding/melanomaapp/view/UserActivity.kt
@@ -0,0 +1,14 @@
+package com.dicoding.melanomaapp.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dicoding.melanomaapp.R
+
+class UserActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
+    }
+
+}
