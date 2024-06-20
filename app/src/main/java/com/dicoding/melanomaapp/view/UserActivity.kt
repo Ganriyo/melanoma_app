f426ea5bfe267abff9206e6b2113a1009d18089f
Ganriyo <672020051@student.uksw.edu>
2024-06-20 19:52:19 +0700

    Update UserActivity

--- app/src/main/java/com/dicoding/melanomaapp/view/UserActivity.kt
+++ app/src/main/java/com/dicoding/melanomaapp/view/UserActivity.kt
@@ -1,6 +1,8 @@
 package com.dicoding.melanomaapp.view
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.dicoding.melanomaapp.R
 
@@ -9,6 +11,25 @@ class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
-    }
 
+        val homeButton = findViewById<ImageView>(R.id.homeButton)
+        val historyButton = findViewById<ImageView>(R.id.historyButton)
+        val profileButton = findViewById<ImageView>(R.id.profileButton)
+
+        homeButton.setOnClickListener{
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        historyButton.setOnClickListener{
+            val intent = Intent(this,HistoryActivity::class.java)
+            startActivity(intent)
+        }
+
+        profileButton.setOnClickListener {
+            val intent = Intent(this, UserActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
 }
